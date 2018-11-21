74d551b51acf0104ab8c2b7a292b8162b06ce28f
Joel Worrall <jworrall@newrelic.com>
2018-11-21 00:07:50 -0500

    initial checkin of monitoring lab.

---
+++ dbutil.js
@@ -0,0 +1,271 @@
+const newrelic = require('newrelic')
+const { users, locations, bagelTypes } = require('./testdata')
+const uuid = require('uuid')
+const startCase = require('lodash.startcase')
+//lokijs documentation https://rawgit.com/techfort/LokiJS/master/jsdoc/index.html
+const loki = require('lokijs')
+
+class Order {
+    constructor(id, {request_date, location, items, account, source_system}) {
+        this.id = id;
+        this.request_date = request_date;
+        this.location = location;
+        this.source_system = source_system;
+        this.account = account; //object
+        this.items = items; //array of objects
+    }
+}
+class Account {
+    constructor(id, {email, cell, name}) {
+        this.id = id;
+        this.email = email;
+        this.cell = cell;
+        this.name = name;
+    }
+}
+const loadOrder = async (id) => {
+    return new Promise(resolve => {
+        newrelic.startSegment('loadOrderById', false, () => {
+            const orders = db.getCollection("orders")
+            const accounts = db.getCollection("accounts")
+            //Object.assign to ensure we're not creating circular references
+            const order = Object.assign({}, orders.findOne( {'id':id}))
+            if (order) {
+                const account = accounts.findOne( { 'id': order.account_id })
+                order.customer = account
+                order.dateLastOpened = new Date().getTime()
+                orders.update(order)
+            } 
+            resolve(order)
+        })
+    })
+}
+
+const queryOrders = async (args) => {
+    const { query, location, bagel } = args    
+    if (location) {
+        return new Promise(resolve => {
+            newrelic.startSegment('queryOrdersByLocation', false, () => {
+                const orders = db.getCollection("orders")
+                const accounts = db.getCollection("accounts")
+                const results = orders.find({ 'location': location })
+                results.forEach(element => {
+                    if (element.account_id) {
+                        //Object.assign to ensure we're not creating circular references
+                        element.customer = Object.assign({}, accounts.findOne({ 'id': element.account_id}))
+                    }
+                })
+                resolve(results)        
+            })
+        })
+    } else if (bagel) {
+        return new Promise(resolve => {
+            newrelic.startSegment('queryOrdersByBagelType', false, () => {
+                const orders = db.getCollection("orders")
+                const accounts = db.getCollection("accounts")
+                const results = orders.find({ 'items.type': { '$contains': bagel } })
+                results.forEach(element => {
+                    if (element.account_id) {
+                        //Object.assign to ensure we're not creating circular references
+                        element.customer = Object.assign({}, accounts.findOne({ 'id': element.account_id}))
+                    }
+                })
+                resolve(results)        
+            })
+        })
+    } else if (query) {
+        return new Promise(resolve => {
+            newrelic.startSegment('queryOrdersBySearchTerm', false, () => {
+                const orders = db.getCollection("orders")
+                const accounts = db.getCollection("accounts")
+                const ids = orders.find({
+                    '$or': [
+                            { 
+                                'id' : {
+                                    '$contains': query
+                                }
+                            },{
+                                'request_date' : {
+                                    '$contains': query
+                                }
+                            },{
+                                'location' : {
+                                    '$contains': query
+                                }
+                            },{
+                                'source' : {
+                                    '$contains': query
+                                }
+                            },{
+                                'items.type' : {
+                                    '$contains': query
+                                }
+                            },{
+                                'items.label' : {
+                                    '$contains': query
+                                }
+                            }
+                    ]
+                })
+                const results = ids.map((result) => {
+                    return loadOrder(result.id)
+                })
+                resolve(results)
+            })
+        })
+    } else {
+        return new Promise(resolve => {
+            newrelic.startSegment('queryOrdersOpen', false, () => {
+                const orders = db.getCollection("orders")
+                const accounts = db.getCollection("accounts")
+                //return the entire database... this is probably going to take a moment.
+                const results = orders.chain().data()
+                results.forEach(element => {
+                    if (element.account_id) {
+                        //Object.assign to ensure we're not creating circular references
+                        element.customer = Object.assign({}, accounts.findOne({ 'id': element.account_id}))
+                    }
+                })    
+                resolve(results)        
+            })
+        })
+    }
+}
+const upsertOrder = async (input) => {
+    if (input.id) {
+        return new Promise(resolve => {
+            newrelic.startSegment('updateOrder', false, () => {
+                const orders = db.getCollection("orders")
+                var order = orders.findOne({ 'id': input.id })
+                order = Object.assign(order, input)        
+                orders.update(order)
+                resolve(order)        
+            })
+        })
+    } else {
+        return new Promise(resolve => {
+            newrelic.startSegment('insertOrder', false, () => {
+                const orders = db.getCollection("orders")
+                input.id = uuid()
+                input.dateLastOpened = null
+                resolve(orders.insert(input))
+            })
+        })
+    }
+}
+const loadAccount = async (id) => {
+    return new Promise(resolve => {
+        newrelic.startSegment('loadAccount', false, () => {
+            const accounts = db.getCollection("accounts")
+            const orders = db.getCollection("orders")
+            //Object.assign to ensure we're not creating circular references
+            const account = Object.assign({}, accounts.findOne( {'id':id} ))
+            const myOrders = orders.find({ 'account_id':id })
+            account.orders = myOrders
+            resolve(account)
+        })
+    })    
+}
+const queryAccounts = async (query) => {    
+    if (query) {
+        return new Promise(resolve => {
+            newrelic.startSegment('queryAccountsBtSearchTerm', false, () => {
+                const accountsCollection = db.getCollection("accounts")
+                const accounts = accountsCollection.find({'$or': [{ 
+                    'email' : {
+                        '$contains': query
+                    }
+                  },{
+                    'name' : {
+                        '$contains': query
+                    }
+                  }]})
+                resolve(accounts)
+            })
+        })    
+    } else {
+        return new Promise(resolve => {
+            newrelic.startSegment('queryAccountsBtSearchTerm', false, () => {
+                const accountsCollection = db.getCollection("accounts")
+                resolve(accountsCollection.chain().simplesort("name").data())
+            })
+        })        
+    }
+}
+
+const upsertAccount = async (input) => {
+    if (input.id) {
+        return new Promise(resolve => {
+            newrelic.startSegment('updateAccount', false, () => {
+                const accounts = db.getCollection("accounts")
+                var account = accounts.findOne({ 'id': input.id })
+                account = Object.assign(account, input)
+                accounts.update(account)
+                resolve(account)
+            })
+        })        
+    } else {
+        return new Promise(resolve => {
+            newrelic.startSegment('insertAccount', false, () => {
+                const accounts = db.getCollection("accounts")
+                input.id = uuid()
+                resolve(accounts.insert(input))
+            })
+        })        
+    }
+}
+const deleteAccount = async (id) => {
+    return new Promise(resolve => {
+        newrelic.startSegment('insertAccount', false, () => {
+            const accounts = db.getCollection("accounts")
+            var account = accounts.findOne({ 'id': id})
+            accounts.remove(account)
+            resolve()
+        })
+    })        
+}
+
+const db = new loki('bagels.db', {
+	autoload: true,
+	autoloadCallback : _dbInit,
+	autosave: true, 
+	autosaveInterval: 4000
+})
+
+function _dbInit() {
+    var accounts = db.getCollection("accounts")
+    if (!accounts) {
+        accounts = db.addCollection("accounts")
+        users.forEach(user => {
+            upsertAccount(user)
+        })
+        db.saveDatabase()
+    }
+
+    var orders = db.getCollection("orders")
+    if (!orders) {
+        orders = db.addCollection("orders")
+        for (var i = 0; i < 100000; i++) {
+            const location = locations[Math.floor((Math.random() * locations.length))]
+            const bagelType = bagelTypes[Math.floor((Math.random() * bagelTypes.length))]
+            const account_id = users[Math.floor((Math.random() * users.length))].id
+            const order = {
+                request_date: new Date('2018', '11', Math.floor((Math.random() * 30)+1)),
+                location: location,
+                items: [ {
+                    label: startCase(bagelType),
+                    type: bagelType,
+                    quantity: Math.floor((Math.random() * 5)+1),
+                    perUnitPrice: 2.50
+                }],
+                account_id: account_id,
+                source_system: 'dataload'
+            }
+            upsertOrder(order)
+        } 
+    }
+    db.saveDatabase()
+    console.log(`Accounts: ${accounts.count()} Orders: ${orders.count()}`)
+}
+
+module.exports = { loadOrder, queryOrders, upsertOrder, loadAccount, upsertAccount, deleteAccount, queryAccounts, Order, Account };
---
+++ server.js
@@ -0,0 +1,151 @@
+const newrelic = require('newrelic')
+const { ApolloServer, gql } = require('apollo-server')
+const { loadOrder, queryOrders, upsertOrder, loadAccount, upsertAccount, deleteAccount, queryAccounts } = require('./dbutil')
+
+// GraphQL schema
+const getOrder = (parent, args, context, info) => { 
+    var id = args.id
+    return new Promise(resolve => {
+        newrelic.startSegment('getOrder', false, () => {
+            loadOrder(id).then(order => {
+                console.log("resolving order", order)
+                resolve(order)
+            })
+        })
+    })
+}
+const getOrders = (parent, args, context, info) => {
+    return new Promise(resolve => {
+        newrelic.startSegment('getOrders', false, () => {
+            queryOrders(args).then(results => {
+                resolve(results)
+            })
+        })
+    })
+}
+const createOrder = (parent, args, context, info) => {
+    var input = args.input
+    if (!input.request_date)
+        input.request_date = new Date()
+    return new Promise(resolve => {
+        newrelic.startSegment('createOrder', false, () => {
+            resolve(upsertOrder(input))
+        })
+    })
+}
+const crudAccount = (parent, args, context, info) => {
+    var input = args.input
+    input.mutation = input.mutation.toLowerCase()
+    return new Promise(resolve => {
+        newrelic.startSegment('crudAccount', false, () => {
+            if (input.mutation == "delete") {
+                resolve(deleteAccount(input))
+            } else {
+                resolve(upsertAccount(input))
+            }                    
+        })
+    })
+}
+const getAccount = (parent, args, context, info) => {
+    return new Promise(resolve => {
+        newrelic.startSegment('getAccount', false, () => {
+            resolve(loadAccount(args.id))
+        })
+    })
+}
+const getAccounts = (parent, args, context, info) => {
+    return new Promise(resolve => {
+        newrelic.startSegment('getAccounts', false, () => {
+            resolve(queryAccounts(args.query))
+        })
+    })
+}
+
+// The GraphQL schema
+const typeDefs = gql`
+scalar Date
+type Query {
+    account(id: String!): AccountHistory
+    accounts(query: String): [Account]
+    order(id: String!): Order
+    orders(query: String, location: String, bagel: String
+    ): [Order]
+}
+type Mutation {
+    mutateAccount(input: AccountInput!): AccountHistory
+    addOrder(input: OrderInput!): Order
+}  
+input OrderInput {
+    request_date: Date
+    location: String! 
+    items: [ItemInput]!
+    account_id: String
+    source_system: String!
+}
+input AccountInput {
+    id: String
+    email: String
+    cell: String
+    name: String
+    mutation: String
+}
+input ItemInput {
+    label: String
+    type: String
+    quantity: Int
+    perUnitPrice: Float
+}  
+type Order {
+    id: String
+    request_date: Date
+    location: String
+    items: [OrderItem]
+    account_id: String
+    customer: Account
+    source: String
+    dateLastOpened: Date
+}
+type OrderItem {
+    label: String
+    type: String
+    quantity: Int
+    perUnitPrice: Float
+}
+type Account {
+    id: String
+    email: String
+    cell: String
+    name: String
+}
+type AccountHistory {
+    id: String
+    email: String
+    cell: String
+    name: String
+    orders: [Order]
+}`
+
+// A map of functions which return data for the schema.
+const resolvers = {
+    Query: {
+        order: getOrder,
+        orders: getOrders,
+        account: getAccount,
+        accounts: getAccounts
+    
+    },
+    Mutation: {
+        addOrder: createOrder,
+        mutateAccount: crudAccount  
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`)
+});
